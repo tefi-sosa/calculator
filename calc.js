9450da26d687afa521d2255a201a81a3b028b558
Estefania Sosa <estefi.monumental@gmail.com>
2022-07-12 16:09:58 -0400

    Deleted comments

--- calc.js
+++ calc.js
@@ -26,11 +26,6 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(Math.sqrt(num1, num2));
 	}
 
-	// console.log('mathSymbol', mathSymbol);
-	// console.log('num1', num1);
-	// console.log('num2', num2);
-
-
 	// This line closes the connection to the command line interface.
 	reader.close()
 
